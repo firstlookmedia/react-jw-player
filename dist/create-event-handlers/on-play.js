7b50490ac5f0357410629a9f9c841bd8619bc84c
jcseo3 <jseo@colgate.edu>
2017-09-28 15:05:25 -0400

    pass duration in video plays as prop

--- dist/create-event-handlers/on-play.js
+++ dist/create-event-handlers/on-play.js
@@ -4,15 +4,16 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 function onPlay(event) {
+  var duration = window.jwplayer(this.props.playerId).getDuration();
   if (event.playReason === 'autostart') {
     this.setState({
       hasPlayed: true
     });
-    this.props.onAutoStart(event);
+    this.props.onAutoStart(event, duration);
   } else if (this.state.hasPlayed && event.oldstate === 'paused') {
     this.props.onResume(event);
   } else {
-    this.props.onPlay(event);
+    this.props.onPlay(event, duration);
     this.setState({
       hasPlayed: true
     });
--- src/create-event-handlers/on-play.js
+++ src/create-event-handlers/on-play.js
@@ -1,13 +1,14 @@
 function onPlay(event) {
+  const duration = window.jwplayer(this.props.playerId).getDuration();
   if (event.playReason === 'autostart') {
     this.setState({
       hasPlayed: true,
     });
-    this.props.onAutoStart(event);
+    this.props.onAutoStart(event, duration);
   } else if (this.state.hasPlayed && event.oldstate === 'paused') {
     this.props.onResume(event);
   } else {
-    this.props.onPlay(event);
+    this.props.onPlay(event, duration);
     this.setState({
       hasPlayed: true,
     });
